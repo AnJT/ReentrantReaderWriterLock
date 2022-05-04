68db15d92068aa5e6ca6d554f74f2d67f1087fd7
番茄炒鸡蛋 <342472121@qq.com>
2022-05-04 19:33:23 +0800

    bug

--- ReadWriteLock/TestCase2.cs
+++ ReadWriteLock/TestCase2.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace ReadWriteLock;
-/**
+namespace ReadWriteLock
+{
+    /**
  * 可重入读写锁性能测试
  * ReaderWriterLockSlim作为 Baseline做性能比较
  *
@@ -25,185 +24,188 @@ namespace ReadWriteLock;
  * 
  * 小优
  */
-public class TestCase2
-{
-    private long readWaitTime;
-    private long writeWaitTime;
-
-    private int readerThreadNum;
-    private int writerThreadNum;
-    private int totalThreadNum;
-
-    private int finishedWorkerCount;
-    private AutoResetEvent finished;
-
-    private ReentrantReaderWriterLock rwLock;
-    private ReaderWriterLockSlim rwLockSlim;
-
-    public TestCase2()
+    public class TestCase2
     {
-        readWaitTime = 0;
-        writeWaitTime = 0;
+        private long readWaitTime;
+        private long writeWaitTime;
 
-        readerThreadNum = 0;
-        writerThreadNum = 0;
-        totalThreadNum = 1024;
+        private int readerThreadNum;
+        private int writerThreadNum;
+        private int totalThreadNum;
 
-        finishedWorkerCount = 0;
-        finished = new AutoResetEvent(false);
-        
-        rwLock = new ReentrantReaderWriterLock();
-        rwLockSlim = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
-    }
+        private int finishedWorkerCount;
+        private AutoResetEvent finished;
 
-    private static void Reader(TestCase2 testCase)
-    {
-        Stopwatch stopwatch = new Stopwatch();
-        // 记录获取读锁的等待时间
-        stopwatch.Start();
-        testCase.rwLock.EnterReadLock();
-        stopwatch.Stop();
-        // 原子操作，更新读者等待总时间
-        Interlocked.Add(ref testCase.readWaitTime, stopwatch.ElapsedMilliseconds);
-        // 模仿读操作用时
-        Thread.Sleep(10);
-        // 释放读锁
-        testCase.rwLock.ExitReadLock();
-        Interlocked.Increment(ref testCase.finishedWorkerCount);
-        if (testCase.finishedWorkerCount == testCase.totalThreadNum)
+        private ReentrantReaderWriterLock rwLock;
+        private ReaderWriterLockSlim rwLockSlim;
+
+        public TestCase2()
         {
-            testCase.finished.Set();
+            readWaitTime = 0;
+            writeWaitTime = 0;
+
+            readerThreadNum = 0;
+            writerThreadNum = 0;
+            totalThreadNum = 1024;
+
+            finishedWorkerCount = 0;
+            finished = new AutoResetEvent(false);
+
+            rwLock = new ReentrantReaderWriterLock();
+            rwLockSlim = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
         }
-    }
 
-    private static void Writer(TestCase2 testCase)
-    {
-        Stopwatch stopwatch = new Stopwatch();
-        // 记录获取写锁的等待时间
-        stopwatch.Start();
-        testCase.rwLock.EnterWriteLock();
-        stopwatch.Stop();
-        // 原子操作，更新写者等待总时间
-        Interlocked.Add(ref testCase.writeWaitTime, stopwatch.ElapsedMilliseconds);
-        // 模仿写操作用时
-        Thread.Sleep(100);
-        // 释放写锁
-        testCase.rwLock.ExitWriteLock();
-        Interlocked.Increment(ref testCase.finishedWorkerCount);
-        if (testCase.finishedWorkerCount == testCase.totalThreadNum)
+        private static void Reader(TestCase2 testCase)
         {
-            testCase.finished.Set();
-        }
-    }
-    
-    private static void ReaderBaseline(TestCase2 testCase)
-    {
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-        testCase.rwLockSlim.EnterReadLock();
-        stopwatch.Stop();
-        // 原子操作，更新读者等待总时间
-        Interlocked.Add(ref testCase.readWaitTime, stopwatch.ElapsedMilliseconds);
-        // 模仿读操作用时
-        Thread.Sleep(10);
-        testCase.rwLockSlim.ExitReadLock();
-        Interlocked.Increment(ref testCase.finishedWorkerCount);
-        if (testCase.finishedWorkerCount == testCase.totalThreadNum)
-        {
-            testCase.finished.Set();
-        }
-    }
-    
-    private static void WriterBaseline(TestCase2 testCase)
-    {
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-        testCase.rwLockSlim.EnterWriteLock();
-        stopwatch.Stop();
-        // 原子操作，更新写者等待总时间
-        Interlocked.Add(ref testCase.writeWaitTime, stopwatch.ElapsedMilliseconds);
-        // 模仿写操作用时
-        Thread.Sleep(100);
-        testCase.rwLockSlim.ExitWriteLock();
-        Interlocked.Increment(ref testCase.finishedWorkerCount);
-        if (testCase.finishedWorkerCount == testCase.totalThreadNum)
-        {
-            testCase.finished.Set();
-        }
-    }
-    
-    private void PrintTestResult(Stopwatch stopwatch, String lockName)
-    {
-        Console.WriteLine(lockName + "所耗总时间{0}ms", stopwatch.ElapsedMilliseconds);
-        Console.WriteLine(lockName + "读者等待时间：{0}ms，" + lockName + "写者等待时间{1}ms", readWaitTime, writeWaitTime);
-        Console.WriteLine(lockName + "读者平均等待时间：{0}ms，" + lockName + "写者平均等待时间{1}ms", readWaitTime / readerThreadNum, writeWaitTime / writerThreadNum);
-    }
-
-    public void Test()
-    {
-        System.Console.WriteLine("\nTest Case 2 start!\n");
-        
-        // 使用stopwatch计算耗时
-        Stopwatch stopwatch = new Stopwatch();
-        var rand = new Random();
-        int[] randNumList = new int[totalThreadNum];
-        // 使用随机数，模拟5%的线程为写者线程，同时记录随机数以保证两次测试的公平性
-        for (int i = 0; i < totalThreadNum; i++)
-            randNumList[i] = rand.Next(20);
-        
-        // 使用我们自己实现的 ReentrantReaderWriterLock 进行测试
-        stopwatch.Start();
-        for (int i = 0; i < totalThreadNum; i++)
-        {
-            int randNum = randNumList[i];
-            // randNum范围是0-19，5%的线程为写者
-            if (randNum == 0)
+            Stopwatch stopwatch = new Stopwatch();
+            // 记录获取读锁的等待时间
+            stopwatch.Start();
+            testCase.rwLock.EnterReadLock();
+            stopwatch.Stop();
+            // 原子操作，更新读者等待总时间
+            Interlocked.Add(ref testCase.readWaitTime, stopwatch.ElapsedMilliseconds);
+            // 模仿读操作用时
+            Thread.Sleep(10);
+            // 释放读锁
+            testCase.rwLock.ExitReadLock();
+            Interlocked.Increment(ref testCase.finishedWorkerCount);
+            if (testCase.finishedWorkerCount == testCase.totalThreadNum)
             {
-                writerThreadNum++;
-                new Thread(() => Writer(this)).Start();
-            }
-            else
-            {
-                readerThreadNum++;
-                new Thread(() => Reader(this)).Start();
+                testCase.finished.Set();
             }
         }
 
-        finished.WaitOne();
-        stopwatch.Stop();
-        // 输出测试结果
-        PrintTestResult(stopwatch, "Ours");
-        
-        // 初始化统计量
-        readerThreadNum = 0;
-        writerThreadNum = 0;
-        readWaitTime = 0;
-        writeWaitTime = 0;
-        finishedWorkerCount = 0;
-        stopwatch = new Stopwatch();
-
-        // 测试使用来完成读写任务，作为测试的BaseLine;
-        stopwatch.Start();
-        for (int i = 0; i < totalThreadNum; i++)
+        private static void Writer(TestCase2 testCase)
         {
-            int randNum = randNumList[i];
-            // randNum范围是0-19，5%的线程为写者
-            if (randNum == 0)
+            Stopwatch stopwatch = new Stopwatch();
+            // 记录获取写锁的等待时间
+            stopwatch.Start();
+            testCase.rwLock.EnterWriteLock();
+            stopwatch.Stop();
+            // 原子操作，更新写者等待总时间
+            Interlocked.Add(ref testCase.writeWaitTime, stopwatch.ElapsedMilliseconds);
+            // 模仿写操作用时
+            Thread.Sleep(100);
+            // 释放写锁
+            testCase.rwLock.ExitWriteLock();
+            Interlocked.Increment(ref testCase.finishedWorkerCount);
+            if (testCase.finishedWorkerCount == testCase.totalThreadNum)
             {
-                writerThreadNum++;
-                new Thread(() => WriterBaseline(this)).Start();
-            }
-            else
-            {
-                readerThreadNum++;
-                new Thread(() => ReaderBaseline(this)).Start();
+                testCase.finished.Set();
             }
         }
-        finished.WaitOne();
-        stopwatch.Stop();
-        // 输出测试结果
-        PrintTestResult(stopwatch, "Baseline");
-        
-        System.Console.WriteLine("\nTest Case 2 end!\n");
+
+        private static void ReaderBaseline(TestCase2 testCase)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            testCase.rwLockSlim.EnterReadLock();
+            stopwatch.Stop();
+            // 原子操作，更新读者等待总时间
+            Interlocked.Add(ref testCase.readWaitTime, stopwatch.ElapsedMilliseconds);
+            // 模仿读操作用时
+            Thread.Sleep(10);
+            testCase.rwLockSlim.ExitReadLock();
+            Interlocked.Increment(ref testCase.finishedWorkerCount);
+            if (testCase.finishedWorkerCount == testCase.totalThreadNum)
+            {
+                testCase.finished.Set();
+            }
+        }
+
+        private static void WriterBaseline(TestCase2 testCase)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            testCase.rwLockSlim.EnterWriteLock();
+            stopwatch.Stop();
+            // 原子操作，更新写者等待总时间
+            Interlocked.Add(ref testCase.writeWaitTime, stopwatch.ElapsedMilliseconds);
+            // 模仿写操作用时
+            Thread.Sleep(100);
+            testCase.rwLockSlim.ExitWriteLock();
+            Interlocked.Increment(ref testCase.finishedWorkerCount);
+            if (testCase.finishedWorkerCount == testCase.totalThreadNum)
+            {
+                testCase.finished.Set();
+            }
+        }
+
+        private void PrintTestResult(Stopwatch stopwatch, String lockName)
+        {
+            Console.WriteLine(lockName + "所耗总时间{0}ms", stopwatch.ElapsedMilliseconds);
+            Console.WriteLine(lockName + "读者等待时间：{0}ms，" + lockName + "写者等待时间{1}ms", readWaitTime, writeWaitTime);
+            Console.WriteLine(lockName + "读者平均等待时间：{0}ms，" + lockName + "写者平均等待时间{1}ms", readWaitTime / readerThreadNum,
+                writeWaitTime / writerThreadNum);
+        }
+
+        public void Test()
+        {
+            System.Console.WriteLine("\nTest Case 2 start!\n");
+
+            // 使用stopwatch计算耗时
+            Stopwatch stopwatch = new Stopwatch();
+            var rand = new Random();
+            int[] randNumList = new int[totalThreadNum];
+            // 使用随机数，模拟5%的线程为写者线程，同时记录随机数以保证两次测试的公平性
+            for (int i = 0; i < totalThreadNum; i++)
+                randNumList[i] = rand.Next(20);
+
+            // 使用我们自己实现的 ReentrantReaderWriterLock 进行测试
+            stopwatch.Start();
+            for (int i = 0; i < totalThreadNum; i++)
+            {
+                int randNum = randNumList[i];
+                // randNum范围是0-19，5%的线程为写者
+                if (randNum == 0)
+                {
+                    writerThreadNum++;
+                    new Thread(() => Writer(this)).Start();
+                }
+                else
+                {
+                    readerThreadNum++;
+                    new Thread(() => Reader(this)).Start();
+                }
+            }
+
+            finished.WaitOne();
+            stopwatch.Stop();
+            // 输出测试结果
+            PrintTestResult(stopwatch, "Ours");
+
+            // 初始化统计量
+            readerThreadNum = 0;
+            writerThreadNum = 0;
+            readWaitTime = 0;
+            writeWaitTime = 0;
+            finishedWorkerCount = 0;
+            stopwatch = new Stopwatch();
+
+            // 测试使用来完成读写任务，作为测试的BaseLine;
+            stopwatch.Start();
+            for (int i = 0; i < totalThreadNum; i++)
+            {
+                int randNum = randNumList[i];
+                // randNum范围是0-19，5%的线程为写者
+                if (randNum == 0)
+                {
+                    writerThreadNum++;
+                    new Thread(() => WriterBaseline(this)).Start();
+                }
+                else
+                {
+                    readerThreadNum++;
+                    new Thread(() => ReaderBaseline(this)).Start();
+                }
+            }
+
+            finished.WaitOne();
+            stopwatch.Stop();
+            // 输出测试结果
+            PrintTestResult(stopwatch, "Baseline");
+
+            System.Console.WriteLine("\nTest Case 2 end!\n");
+        }
     }
 }
